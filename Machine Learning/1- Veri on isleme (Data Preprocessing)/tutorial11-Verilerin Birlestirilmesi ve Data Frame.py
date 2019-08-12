d9790ff988f82f0ef6d8b3dd0a2a098cd34d2899
fbasatemur <51318402+fbasatemur@users.noreply.github.com>
2019-08-12 10:45:58 +0300

    Update tutorial11-Verilerin Birlestirilmesi ve Data Frame.py

--- Machine Learning/1- Veri on isleme (Data Preprocessing)/tutorial11-Verilerin Birlestirilmesi ve Data Frame.py
+++ Machine Learning/1- Veri on isleme (Data Preprocessing)/tutorial11-Verilerin Birlestirilmesi ve Data Frame.py
@@ -12,10 +12,10 @@ from sklearn.preprocessing import Imputer # eksik veriler icin
 # eksikleri doldurma yontemi -> 'mean'
 imputer = Imputer(missing_values="NaN",strategy = "mean",axis=0)
 
-Yasboykilo = eksikveriler.iloc[:,1:4].values       # yalnizca sayisal olan kolonlar alinir [satir:satir,kolon:kolon]
+Yasboykilo = eksikveriler.iloc[:,1:4].values              # 1. 2. ve 3. kolonlari dilimle
 print(Yasboykilo)
-imputer = imputer.fit(Yasboykilo[:,1:4])   # her kolon icin ort hesapla
-Yasboykilo[:,1:4] = imputer.transform(Yasboykilo[:,1:4])  # parametre degisikligini uygula
+imputer = imputer.fit(Yasboykilo[:,1:4])                  # her kolon icin ort hesapla
+Yasboykilo[:,1:4] = imputer.transform(Yasboykilo[:,1:4])  # 1. 2. ve 3. satirdaki eksik verileri kolon ortalamalri ile doldur
 print(Yasboykilo)
 
 # categorical -> numeric transform
@@ -23,25 +23,25 @@ from sklearn.preprocessing import OneHotEncoder
 
 ulke = veriler.iloc[:,0:1].values
 ohe = OneHotEncoder(categorical_features = "all")
-ulke= ohe.fit_transform(ulke[:,0:1]).toarray()      # nominal degrelerden kolon bazli degerler olusturuldu
+ulke= ohe.fit_transform(ulke[:,0:1]).toarray()            # nominal degrelerden kolon bazli degerler olusturuldu
 
 print(ulke)
 
-# print(list(range(22)))    0-21 sayilari verir
-
+# print(list(range(22)))    0-21 sayilarindan olusan liste verir
+# columns -> kolon basliklari
 sonuc = pd.DataFrame(data = ulke, index = range(22), columns = ['fr','tr','us'])     # OneHotEncoder verilerinden dataframe olusturulur
 print (sonuc)
 
 sonuc2 = pd.DataFrame(data = Yasboykilo, index = range(22), columns = ['boy','kilo','yas'])
 print(sonuc2)
 
-cinsiyet = veriler.iloc[:,-1:].values
+cinsiyet = veriler.iloc[:,-1:].values       # -1. yani son kolon (bagimli degerleri icerir)
 
 print(cinsiyet)
 sonuc3 = pd.DataFrame(data = cinsiyet, index=range(22),columns = ['cinsiyet'])
 print(sonuc3)
 
-# kolonlari birlestir
+# concat ile kolonlari birlestir
 
 s = pd.concat([sonuc,sonuc2],axis = 1)
 print(s)
