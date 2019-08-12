87baba93cc5c57f25a79fb43a5cd465050e2110b
fbasatemur <51318402+fbasatemur@users.noreply.github.com>
2019-08-12 10:26:30 +0300

    Update tutorial9-Eksik veriler.py

--- Machine Learning/1- Veri on isleme (Data Preprocessing)/tutorial9-Eksik veriler.py
+++ Machine Learning/1- Veri on isleme (Data Preprocessing)/tutorial9-Eksik veriler.py
@@ -2,26 +2,29 @@
 """
 Created on Thu Jun 27 00:03:02 2019
 
-@author: fatih
+.csv veri kumsesindeki 'NaN' ile ifade edilen eksik verileri, verinin bulundugu kolonun ortalamasi ile 
+doldurarak eksik veri problemini ortadan kaldirabiliriz.
 """
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# veri yukleme
+# veri yuklemesi
 
 eksikveriler = pd.read_csv('eksik_veriler.csv')
 
 print(eksikveriler)
 # csv dosyasindaki eksik verileri tablo ortalamalari ile doldur
 
-    # sci-kit learn --->
+# sci-kit learn ---> sklearn
 from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values="NaN",strategy = "mean",axis=0)
-Yas = eksikveriler.iloc[:,1:4].values       # yalnizca sayisal olan kolonlar alinir [satir:satir,kolon:kolon]
+imputer = Imputer(missing_values="NaN", strategy = "mean", axis=0)
+Yas = eksikveriler.iloc[:,1:4].values       # yalnizca sayisal olan kolonlar alinir [satir:satir,kolon:kolon]  
+# [:,1:4]-> 1. kolondan 4. kolona kadar (4 dahil degil)
+# .values kolon basliklarini almamak icin array donusumudur
+
 print(Yas)
-imputer = imputer.fit(Yas[:,1:4])   # her kolon icin ort hesapla
+imputer = imputer.fit(Yas[:,1:4])   # 1. 2. ve 3. kolonlar icin ort hesapla
 Yas[:,1:4] = imputer.transform(Yas[:,1:4])  # parametre degisikligini uygula
 print(Yas)
 
