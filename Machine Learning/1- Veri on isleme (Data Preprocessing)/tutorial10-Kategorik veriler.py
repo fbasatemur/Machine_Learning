ee169c2f24cff512a0e6816b90c2b9f081ebd512
fbasatemur <51318402+fbasatemur@users.noreply.github.com>
2019-08-12 10:37:27 +0300

    Update tutorial10-Kategorik veriler.py

--- Machine Learning/1- Veri on isleme (Data Preprocessing)/tutorial10-Kategorik veriler.py
+++ Machine Learning/1- Veri on isleme (Data Preprocessing)/tutorial10-Kategorik veriler.py
@@ -2,7 +2,10 @@
 """
 Created on Thu Jun 27 21:57:00 2019
 
-@author: fatih
+Veri kumemizde kategorik veriler de bulunabilir. Bu verileri numeric verilere donusturmemiz gerekir.
+Bunu icin sklearn den LabelEncoder ve OneHotEncoder i kullanbiliriz 
+LabelEncoder -> 0 dan baslayarak ayni kolonda bulunan verileri categorical -> numeric donusumunu yapar.
+OneHotEncoder -> Ayni kolonda bulunan categorical verilerin her birini bir kolon olarak ekler ve 1-0 olarak doldurur.
 """
 
 import numpy as np
@@ -11,17 +14,18 @@ import matplotlib.pyplot as plt
 
 # veri yukleme
 
-veriler = pd.read_csv('veriler.csv')
+datas = pd.read_csv('veriler.csv')
 
 # sci-kit learn ---> sklearn
 from sklearn.preprocessing import Imputer
-# veriler ' den nominal veri kolonu olan ulke kolonu numeric degerlendirme icin dilimleniyor
-ulke = veriler.iloc[:,0:1].values
+
+# alinan veriler, nominal veri kolonu olan ulke kolonunu numeric degerlendirme icin dilimleniyor
+ulke = datas.iloc[:,0:1].values
 print(ulke)
 
 from sklearn.preprocessing import LabelEncoder      # her bir ifadeye sayisal deger atar
 
-le = LabelEncoder()         # le nesnesi tanimlandi
+le = LabelEncoder()                                 # le nesnesi tanimlandi
 ulke[:,0] = le.fit_transform(ulke[:,0]) 
 print(ulke)
                         
